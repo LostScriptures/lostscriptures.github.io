0c1f5c8106a76b08405315ecffc1be82f97a597e
Lost Script <97259974+LostScriptures@users.noreply.github.com>
2025-08-20 11:25:56 +0000

    Tryed something

--- frag.fs
+++ frag.fs
@@ -12,7 +12,7 @@ void main(void) {
     float dist = abs(distance(origin, vPos));
     float pulse = 3.0;
     // dist = dist * 0.5 + 0.5;
-    //vec3 wavy = tan(vec3(uv.x, uv.y, uv.x) * 10.0 + vec3(vTime * 3.0));
+    vec3 wavy = tan(vec3(uv.x, uv.y, uv.x) * 10.0 + vec3(vTime * 3.0));
     if (dist > pulse + 1.0) {
         gl_FragColor = vec4(1.0, 1.0, 1.0 , 1.0);
     
--- vert.vs
+++ vert.vs
@@ -6,7 +6,7 @@ uniform float uTime;
 
 void main(void) {
     vec3 pos = positionAttr;
-    //pos.x += 0.1 * sin(pos.x * 10.0 + uTime * 3.0); // subtle vertical wave
+    pos.x += 0.1 * sin(pos.x * 10.0 + uTime * 3.0); // subtle vertical wave
     //pos.y += 0.1 * cos(pos.y * 10.0 + uTime * 3.0); // subtle horizontal wave
 
     gl_Position = vec4(pos, 1.0);
